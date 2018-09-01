f925d523fd929636f8a46319d68c3a4e28b6be28
Ali Falahi <ali@afalahi.com>
2018-08-31 22:32:21 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# thycotic-ansible-lookup-plugin
-Ansible lookup plugin to retrieve Secrets from Thycotic Secret Server
+# Thycotic Secret Server Ansible Lookup Plugin
+
+This is a lookup plugin for Ansible that works with the Thycotic SDK Python package
+to facilitate the connection to the Thycotic Secret Server
+
+## Prerequisites
+
+Python 2.7* and Python 3.*
+
+Ansible 2.3.1 and later
+
+Thycotic Secret Server Python package installed in Ansible Server environment.
+Installing the Thycotic Secret Server Python package in Puppet Server
+
+``` python
+pip install secret-server-sdk-client
+```
+
+## Deployment
+
+To deploy the lookup plugin, place the lookup python file 'thycotic_secret.py' to the
+lookup folder based on your ansible configurations. Edit the 'thycotic_sdk_config.yml'
+with the appropriate configurations for the sdk and place it in a directory where other
+variable files are located within the environment.
+
+When creating a playbook, make sure to include the 'thycotic_sdk_config.yml' variable file
+in your play when the lookup plugin is being used. __The lookup plugin takes three variables:
+
+- ```thycotic_config``` - name assigned to the 'thycotic_sdk_config.yml' reference
+- ```secret``` - the secret id
+- ```field``` - field slug name
+
+For example,
+
+``` yaml
+  - hosts: localhost
+    vars:
+        contents: "{{ lookup('thycotic_secret', thycotic_config='thycotic', secret=4, field='password'") }}
+    tasks:
+      - include_vars:
+            file: thycotic_sdk_config.yml
+            name: thycotic
+```
+
+## Authors
+
+Paulo Dorado
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+Thycotic Secret Server Python Client
+Thycotic SDK
---
+++ thycotic_secret.py
@@ -0,0 +1,68 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+      lookup: thycotic_sdk
+        author: Paulo Dorado <pdorado@tsiva.com>
+        version_added: "1.0"
+        short_description: Look up secrets from Thycoptic Secret Server
+        description:
+            - This lookup uses the Thycotic SDK python library to lookup secrets in
+        options:
+          _terms:
+            description: path(s) of files to read
+            required: True
+        notes:
+          - if read in variable context, the file can be interpreted as YAML if the content is valid to the parser.
+          - this lookup does not understand 'globing' - use the fileglob lookup instead.
+"""
+
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.module_utils._text import to_text
+from ansible.plugins.lookup import LookupBase
+
+from secret_server.sdk_client import SDK_Client
+
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
+
+class LookupModule(LookupBase):
+    def run(self, terms, variables=None, **kwargs):
+        config_dict = variables[kwargs['thycotic_config']]
+        result = []
+
+        print(kwargs)
+
+        client = SDK_Client()
+        client.configure(config_dict['sdk_client_path'],
+                         config_dict['secret_server_url'],
+                         config_dict['sdk_client_rule'],
+                         config_dict['sdk_client_key'])
+
+        if config_dict['config_reset']:
+            display.vvvv(client.commands.remove())
+
+        display.vvvv(client.commands.initialize())
+
+        try:
+            cache_result = ''
+            if config_dict['sdk_cache_strategy'] == 0:
+                cache_result = client.set_cache(config_dict['sdk_cache_strategy'])
+            else:
+                cache_result = client.set_cache(config_dict['sdk_cache_strategy'], config_dict['sdk_cache_age'])
+
+            display.vvvv(cache_result)
+        except KeyError as e:
+            if e.args[0] == 'sdk_cache_age':
+                KeyError('If cache age is invalid or missing')
+
+        if(config_dict['sdk_cache_clear']):
+            client.commands.clear_cache()
+
+        result.append(to_text(client.commands.get_secret(kwargs['secret'], field=kwargs['field'])))
+
+        return result
